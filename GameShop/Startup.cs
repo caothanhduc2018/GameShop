bac48b4fdcee8afd9be9b7e5e75e2be124bbd8b0
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2022-04-27 13:01:33 +0700

    Update Startup.cs

--- GameShop/Startup.cs
+++ GameShop/Startup.cs
@@ -57,7 +57,7 @@ namespace GameShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=GameChon}/{id?}");
 
             });
             
